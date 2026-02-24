680737ee055162fe4c13b0ef06a3bf5119578898
Anton <yzzzerneime@gmail.com>
2026-02-24 12:46:02 +0800

    Закончил программу, проверил что всё работает

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
 double circl = 5.0;
-double squar = 10.0;
+double squar = 5.0;
 
 if ((squar * squar) > (3.14 * circl * circl)) Console.WriteLine("квадрат больше!");
+else if ((squar * squar) == (3.14 * circl * circl)) Console.WriteLine("Одинаковые!");
+else Console.WriteLine("круг больше!");
