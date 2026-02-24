9b80eac68f2f84e105c481cd6170c085689d7f57
Anton <yzzzerneime@gmail.com>
2026-02-24 13:02:49 +0800

    проверка

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,3 +5,5 @@ double squar = 5.0;
 if ((squar * squar) > (3.14 * circl * circl)) Console.WriteLine("квадрат больше!");
 else if ((squar * squar) == (3.14 * circl * circl)) Console.WriteLine("Одинаковые!");
 else Console.WriteLine("круг больше!");
+
+
